d6aceb070c67561f288d55c11a5638beaea6e9ed
binarymoon <BinaryMoon@gmail.com>
2008-11-21 22:08:01 +0000

    removed Etags - they were breaking png creation
    have also done some general code clean ups

--- timthumb.php
+++ timthumb.php
@@ -14,8 +14,10 @@
 
 // HTML example: <img src="/scripts/timthumb.php?src=/images/whatever.jpg&w=150&h=200&zc=1" alt="" />
 
-if( !isset( $_REQUEST[ "src" ] ) ) {
-	die( "no image specified" );
+error_reporting(E_ALL);
+
+if(!isset($_REQUEST["src"])) {
+	die("no image specified");
 }
 
 // clean params before use
@@ -37,49 +39,57 @@ $quality = preg_replace( "/[^0-9]+/", "", get_request( '9', 80 ) );
 $cache_dir = './cache';
 
 // get mime type of src
-$mime_type = mime_type( $src );
+$mime_type = mime_type($src);
 
 // check to see if this image is in the cache already
-check_cache( $cache_dir, $mime_type );
+//check_cache($cache_dir, $mime_type);
 
 // make sure that the src is gif/jpg/png
-if( !valid_src_mime_type( $mime_type ) ) {
-	$error = "Invalid src mime type: $mime_type";
-	die( $error );
+if(!valid_src_mime_type($mime_type)) {
+	die("Invalid src mime type: $mime_type");
 }
 
 // check to see if GD function exist
 if(!function_exists('imagecreatetruecolor')) {
-	$error = "GD Library Error: imagecreatetruecolor does not exist";
-	die( $error );
+	die("GD Library Error: imagecreatetruecolor does not exist");
 }
 
-if(strlen($src) && file_exists( $src ) ) {
+if(strlen($src) && file_exists($src)) {
 
 	// open the existing image
-	$image = open_image( $mime_type, $src );
-	if( $image === false ) { die( 'Unable to open image : ' . $src ); }		
+	$image = open_image($mime_type, $src);
+	if($image === false) {
+		die('Unable to open image : ' . $src);
+	}
 
 	// Get original width and height
-	$width = imagesx( $image );
-	$height = imagesy( $image );
+	$width = imagesx($image);
+	$height = imagesy($image);
 
 	// don't allow new width or height to be greater than the original
-	if( $new_width > $width ) { $new_width = $width; }
-	if( $new_height > $height ) { $new_height = $height; }
-
-	// generate new w/h if not provided
-	if( $new_width && !$new_height ) {
-		$new_height = $height * ( $new_width / $width );
-	}
-	elseif($new_height && !$new_width) {
-		$new_width = $width * ( $new_height / $height );
-	}
-	elseif(!$new_width && !$new_height) {
+	if( $new_width > $width ) {
 		$new_width = $width;
+	}
+	if( $new_height > $height ) {
 		$new_height = $height;
 	}
 
+	// generate new w/h if not provided
+	if( $new_width && !$new_height ) {
+	
+		$new_height = $height * ( $new_width / $width );
+		
+	} elseif($new_height && !$new_width) {
+	
+		$new_width = $width * ( $new_height / $height );
+		
+	} elseif(!$new_width && !$new_height) {
+	
+		$new_width = $width;
+		$new_height = $height;
+		
+	}
+	
 	// create a new true color image
 	$canvas = imagecreatetruecolor( $new_width, $new_height );
 
@@ -99,8 +109,7 @@ if(strlen($src) && file_exists( $src ) ) {
 			$src_w = round( ( $width / $cmp_x * $cmp_y ) );
 			$src_x = round( ( $width - ( $width / $cmp_x * $cmp_y ) ) / 2 );
 
-		}
-		elseif ( $cmp_y > $cmp_x ) {
+		} elseif ( $cmp_y > $cmp_x ) {
 
 			$src_h = round( ( $height / $cmp_y * $cmp_x ) );
 			$src_y = round( ( $height - ( $height / $cmp_y * $cmp_x ) ) / 2 );
@@ -109,14 +118,13 @@ if(strlen($src) && file_exists( $src ) ) {
         
 		imagecopyresampled( $canvas, $image, 0, 0, $src_x, $src_y, $new_width, $new_height, $src_w, $src_h );
 
-	}
-	else {
+	} else {
 
 		// copy and resize part of an image with resampling
 		imagecopyresampled( $canvas, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
 
 	}
-
+		
 	// output image to browser based on mime type
 	show_image( $mime_type, $canvas, $quality, $cache_dir );
 	
@@ -125,54 +133,54 @@ if(strlen($src) && file_exists( $src ) ) {
 	
 } else {
 
-	if( strlen( $src ) ) {
-		echo $src . ' not found.';
+	if(strlen($src)) {
+		die($src . ' not found.');
 	} else {
-		echo 'no source specified.';
+		die('no source specified.');
 	}
 	
 }
 
-function show_image ( $mime_type, $image_resized, $quality, $cache_dir ) {
+function show_image( $mime_type, $image_resized, $quality, $cache_dir ) {
 
 	// check to see if we can write to the cache directory
 	$is_writable = 0;
 	$cache_file_name = $cache_dir . '/' . get_cache_file();        	
 
-	if( touch( $cache_file_name ) ) {
+	if(touch($cache_file_name)) {
+	
 		// give 666 permissions so that the developer 
 		// can overwrite web server user
-		chmod( $cache_file_name, 0666 );
+		chmod($cache_file_name, 0666);
 		$is_writable = 1;
-	}
-	else {
+		
+	} else {
+	
 		$cache_file_name = NULL;
-		header( 'Content-type: ' . $mime_type );
-	}
-	
-	if( stristr( $mime_type, 'gif' ) ) {
-	
-		imagegif( $image_resized, $cache_file_name );
-		
-	} elseif( stristr( $mime_type, 'jpeg' ) ) {
-	
-		imagejpeg( $image_resized, $cache_file_name, $quality );
-		
-	} elseif( stristr( $mime_type, 'png' ) ) {
-	
-		$quality = $quality / 10;
-		if($quality == 10) {
-			$quality = 9;
-		}
-		imagepng( $image_resized, $cache_file_name, $quality );
+		header('Content-type: ' . $mime_type);
 		
 	}
 	
-	if( $is_writable ) {
+	if(stristr($mime_type, 'gif')) {
+	
+		imagegif($image_resized, $cache_file_name);
+		
+	} elseif(stristr($mime_type, 'jpeg')) {
+	
+		imagejpeg($image_resized, $cache_file_name, $quality);
+		
+	} elseif(stristr($mime_type, 'png')) {
+	
+		$quality = floor($quality * 0.09);		
+		imagepng($image_resized, $cache_file_name, $quality);
+		
+	}
+	
+	if($is_writable) {
 		show_cache_file( $cache_dir, $mime_type );
 	}
-	
-	exit;
+
+	die();
 
 }
 
@@ -186,20 +194,20 @@ function get_request( $property, $default = 0 ) {
 	
 }
 
-function open_image ( $mime_type, $src ) {
+function open_image($mime_type, $src) {
 
-	if( stristr( $mime_type, 'gif' ) ) {
+	if(stristr($mime_type, 'gif')) {
 	
-		$image = imagecreatefromgif( $src );
+		$image = imagecreatefromgif($src);
 		
-	} elseif( stristr( $mime_type, 'jpeg' ) ) {
+	} elseif(stristr($mime_type, 'jpeg')) {
 	
 		@ini_set('gd.jpeg_ignore_warning', 1);
-		$image = imagecreatefromjpeg( $src );
+		$image = imagecreatefromjpeg($src);
 		
-	} elseif( stristr( $mime_type, 'png' ) ) {
+	} elseif( stristr($mime_type, 'png')) {
 	
-		$image = imagecreatefrompng( $src );
+		$image = imagecreatefrompng($src);
 		
 	}
 	
@@ -207,71 +215,76 @@ function open_image ( $mime_type, $src ) {
 
 }
 
-function mime_type ( $file ) {
+function mime_type($file) {
 
     $os = strtolower(php_uname());
 	$mime_type = '';
 
 	// use PECL fileinfo to determine mime type
-	if( function_exists( 'finfo_open' ) ) {
-		$finfo = finfo_open( FILEINFO_MIME );
-		$mime_type = finfo_file( $finfo, $file );
-		finfo_close( $finfo );
+	if( function_exists('finfo_open')) {
+		$finfo = finfo_open(FILEINFO_MIME);
+		$mime_type = finfo_file($finfo, $file);
+		finfo_close($finfo);
 	}
 
 	// try to determine mime type by using unix file command
 	// this should not be executed on windows
-    if( !valid_src_mime_type( $mime_type ) && !(eregi('windows', php_uname()))) {
-		if( preg_match( "/freebsd|linux/", $os ) ) {
-                	$mime_type = trim ( @shell_exec( 'file -bi $file' ) );
+    if(!valid_src_mime_type($mime_type) && !(eregi('windows', $os))) {
+		if(preg_match("/freebsd|linux/", $os)) {
+			$mime_type = trim(@shell_exec('file -bi $file'));
 		}
 	}
 
 	// use file's extension to determine mime type
-	if( !valid_src_mime_type( $mime_type ) ) {
-		$frags = split( "\.", $file );
-		$ext = strtolower( $frags[ count( $frags ) - 1 ] );
+	if(!valid_src_mime_type($mime_type)) {
+
+		// set defaults
+		$mime_type = 'image/jpeg';
+		// file details
+		$fileDetails = pathinfo($file);
+		$ext = strtolower($fileDetails["extension"]);
+		// mime types
 		$types = array(
  			'jpg'  => 'image/jpeg',
  			'jpeg' => 'image/jpeg',
  			'png'  => 'image/png',
  			'gif'  => 'image/gif'
  		);
-		if( strlen( $ext ) && strlen( $types[$ext] ) ) {
-			$mime_type = $types[ $ext ];
+		
+		if(strlen($ext) && strlen($types[$ext])) {
+			$mime_type = $types[$ext];
 		}
-
-		// if no extension provided, default to jpg
-		if( !strlen( $ext ) && !valid_src_mime_type( $mime_type ) ) {
-			$mime_type = 'image/jpeg';
-		}
+		
 	}
+	
 	return $mime_type;
 
 }
 
-function valid_src_mime_type ( $mime_type ) {
+function valid_src_mime_type($mime_type) {
 
-	if( preg_match( "/jpg|jpeg|gif|png/i", $mime_type ) ) { return 1; }
-	return 0;
+	if(preg_match("/jpg|jpeg|gif|png/i", $mime_type)) {
+		return true;
+	}
+	return false;
 
 }
 
-function check_cache ( $cache_dir, $mime_type ) {
+function check_cache($cache_dir, $mime_type) {
 
 	// make sure cache dir exists
-	if( !file_exists( $cache_dir ) ) {
+	if(!file_exists($cache_dir)) {
 		// give 777 permissions so that developer can overwrite
 		// files created by web server user
-		mkdir( $cache_dir );
-		chmod( $cache_dir, 0777 );
+		mkdir($cache_dir);
+		chmod($cache_dir, 0777);
 	}
 
-	show_cache_file( $cache_dir, $mime_type );
+	show_cache_file($cache_dir, $mime_type);
 
 }
 
-function show_cache_file ( $cache_dir, $mime_type ) {
+function show_cache_file($cache_dir, $mime_type) {
 
 	$cache_file = $cache_dir . '/' . get_cache_file();
 
@@ -294,18 +307,19 @@ function show_cache_file ( $cache_dir, $mime_type ) {
 
 		}
 		
-		$fileSize = filesize( $cache_file );
-		
+		$fileSize = filesize($cache_file);
+				
 		// send headers then display image
-		header( "Content-Type: " . $mime_type );
-		header( "Accept-Ranges: bytes" );
-		header( "Last-Modified: " . gmdate( 'D, d M Y H:i:s', filemtime( $cache_file ) ) . " GMT" );
-		header( "Content-Length: " . $fileSize );
-		header( "Cache-Control: max-age=9999, must-revalidate" );
-		header( "Etag: " . md5($fileSize . $gmdate_mod) );						   		
-		header( "Expires: " . gmdate( "D, d M Y H:i:s", time() + 9999 ) . "GMT" );
-		readfile( $cache_file );
-		exit;
+		header("Content-Type: " . $mime_type);
+		//header("Accept-Ranges: bytes");
+		header("Last-Modified: " . gmdate('D, d M Y H:i:s', filemtime($cache_file)) . " GMT");
+		header("Content-Length: " . $fileSize);
+		header("Cache-Control: max-age=9999, must-revalidate");
+		header("Expires: " . gmdate("D, d M Y H:i:s", time() + 9999) . "GMT");
+		
+		readfile($cache_file);
+		
+		die();
 
 	}
 	
