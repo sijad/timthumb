f7d4520047a122e68543616207048ecbadc6fe5d
BinaryMoon <BinaryMoon@c5343e3a-7349-0410-8074-63f643fbca3b>
2010-11-26 23:41:26 +0000

    bump version number

--- timthumb.php
+++ timthumb.php
@@ -13,7 +13,7 @@
 define ('CACHE_SIZE', 250);					// number of files to store before clearing cache
 define ('CACHE_CLEAR', 5);					// maximum number of files to delete on each cache clear
 define ('CACHE_USE', TRUE);					// use the cache files? (mostly for testing)
-define ('VERSION', '1.17');					// version number (to force a cache refresh)
+define ('VERSION', '1.18');					// version number (to force a cache refresh)
 define ('DIRECTORY_CACHE', './cache');		// cache directory
 define ('DIRECTORY_TEMP', './temp');		// temp directory
 define ('MAX_WIDTH', 1000);					// maximum image width
