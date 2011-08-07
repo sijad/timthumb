10362cf485a35e5af8603c5fcca389cdbe63322a
mmaunder@gmail.com <mmaunder@gmail.com@c5343e3a-7349-0410-8074-63f643fbca3b>
2011-08-07 09:28:54 +0000

    set config vars to sane values.

--- trunk/timthumb.php
+++ trunk/timthumb.php
@@ -22,13 +22,13 @@ define ('BLOCK_EXTERNAL_LEECHERS', false);		// If the image or webshot is being 
 define ('ALLOW_EXTERNAL', TRUE);			// Allow image fetching from external websites. Will check against ALLOWED_SITES if ALLOW_ALL_EXTERNAL_SITES is false
 define ('ALLOW_ALL_EXTERNAL_SITES', true);		// Less secure. 
 define ('FILE_CACHE_ENABLED', TRUE);			// Should we store resized/modified images on disk to speed things up?
-define ('FILE_CACHE_TIME_BETWEEN_CLEANS', 10);	// How often the cache is cleaned 
-define ('FILE_CACHE_MAX_FILE_AGE', 10);		// How old does a file have to be to be deleted from the cache
+define ('FILE_CACHE_TIME_BETWEEN_CLEANS', 86400);	// How often the cache is cleaned 
+define ('FILE_CACHE_MAX_FILE_AGE', 86400);		// How old does a file have to be to be deleted from the cache
 define ('FILE_CACHE_SUFFIX', '.timthumb.txt');		// What to put at the end of all files in the cache directory so we can identify them
 define ('FILE_CACHE_DIRECTORY', './cache');		// Directory where images are cached. Left blank it will use the system temporary directory (which is better for security)
 define ('MAX_FILE_SIZE', 10485760);			// 10 Megs is 10485760. This is the max internal or external file size that we'll process.  
 define ('CURL_TIMEOUT', 10);				// Timeout duration for Curl. This only applies if you have Curl installed and aren't using PHP's default URL fetching mechanism.
-define ('WAIT_BETWEEN_FETCH_ERRORS', 10);		//Time to wait between errors fetching remote file
+define ('WAIT_BETWEEN_FETCH_ERRORS', 3600);		//Time to wait between errors fetching remote file
 //Browser caching
 define ('BROWSER_CACHE_MAX_AGE', 864000);		// Time to cache in the browser
 define ('BROWSER_CACHE_DISABLE', false);			// Use for testing if you want to disable all browser caching
@@ -655,7 +655,6 @@ class timthumb {
 			imageconvolution ($canvas, $sharpenMatrix, $divisor, $offset);
 
 		}
-		$this->debug(3, "Tempfile $tempfile");
 		$imgType = "";
 		$tempfile = tempnam($this->cacheDirectory, 'timthumb_tmpimg_');
 		if(preg_match('/^image\/(?:jpg|jpeg)$/i', $mimeType)){ 
@@ -670,7 +669,6 @@ class timthumb {
 		} else {
 			return $this->sanityFail("Could not match mime type after verifying it previously.");
 		}
-		$this->debug(3, "Tempfile $tempfile");
 
 		if(PNGCRUSH_ENABLED){
 			$tempfile2 = tempnam($this->cacheDirectory, 'timthumb_tmpimg_');
