536f71754eff4f221f09769c90bb548e660d327a
binarymoon@gmail.com <binarymoon@gmail.com@c5343e3a-7349-0410-8074-63f643fbca3b>
2012-02-07 13:29:40 +0000

    fix for colour issue to make blues work correctly

--- trunk/timthumb.php
+++ trunk/timthumb.php
@@ -20,7 +20,7 @@
  * loaded by timthumb. This will save you having to re-edit these variables
  * everytime you download a new version
 */
-define ('VERSION', '2.8.5');																		// Version of this script 
+define ('VERSION', '2.8.6');																		// Version of this script 
 //Load a config file if it exists. Otherwise, use the values below
 if( file_exists(dirname(__FILE__) . '/timthumb-config.php'))	require_once('timthumb-config.php');
 if(! defined('DEBUG_ON') )					define ('DEBUG_ON', false);								// Enable debug logging to web server error log (STDERR)
@@ -579,7 +579,7 @@ class timthumb {
 
 		$canvas_color_R = hexdec (substr ($canvas_color, 0, 2));
 		$canvas_color_G = hexdec (substr ($canvas_color, 2, 2));
-		$canvas_color_B = hexdec (substr ($canvas_color, 2, 2));
+		$canvas_color_B = hexdec (substr ($canvas_color, 4, 2));
 
 		// Create a new transparent color for image
 		$color = imagecolorallocatealpha ($canvas, $canvas_color_R, $canvas_color_G, $canvas_color_B, 127);
